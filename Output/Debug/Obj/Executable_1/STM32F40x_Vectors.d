43593e47f29c397abfafc9367db813fbd4e60aeb
Komp0t2 <komp0t@icloud.com>
2020-11-25 13:22:21 +0400

    add gitignore

--- Output/Debug/Obj/Executable_1/STM32F40x_Vectors.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/Executable_1/STM32F40x_Vectors.o: \
- D:\IGOR\STUDY\stm32f407_mpt_lab\STM32F4xx\Source\STM32F40x_Vectors.s
--- Output/Debug/Obj/Executable_1/STM32F4xx_Startup.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/Executable_1/STM32F4xx_Startup.o: \
- D:\IGOR\STUDY\stm32f407_mpt_lab\STM32F4xx\Source\STM32F4xx_Startup.s
